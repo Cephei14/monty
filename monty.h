3183230d703267965381975df8719d2405bccbda
Soufiane ARIF <52210163+soufiixarif@users.noreply.github.com>
2024-03-28 15:41:09 +0000

    Update monty.h

--- monty.h
+++ monty.h
@@ -77,5 +77,6 @@ void s_mul(stack_t **head, unsigned int counter);
 void s_mod(stack_t **head, unsigned int counter);
 void s_pchar(stack_t **head, unsigned int counter);
 void s_pstr(stack_t **head, unsigned int counter);
+int ft_isascii(int c);
 
 #endif
