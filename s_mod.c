b59912ffa7e7d3e68c045c36c59c7523ad20b138
Cephei14 <rafik.dhaibi@gmail.com>
2024-03-28 07:55:35 -0700

    update mod

--- s_mod.c
+++ s_mod.c
@@ -10,10 +10,16 @@
 
 void s_mod(stack_t **head, unsigned int counter)
 {
-	stack_t *top1 = *head;
-	stack_t *top2 = top1->next;
+	stack_t *top1;
+	stack_t *top2;
 	(void)counter;
 
+	if (!(*head) || !(*head)->next)
+		p_error1(11);
+	top1 = *head;
+	top2 = top1->next;
+	if (top1->n == 0)
+		p_error2(15);
 	top2->n = top2->n % top1->n;
 	s_pop(head, 0);
 }
