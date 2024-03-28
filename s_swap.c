2d2c75bba7aa4bb5aa6c4b1e9b4b1d21e2622d15
Cephei14 <rafik.dhaibi@gmail.com>
2024-03-28 07:04:41 -0700

    update swap

--- s_swap.c
+++ s_swap.c
@@ -9,13 +9,16 @@
 
 void s_swap(stack_t **head, unsigned int counter)
 {
-	stack_t *top1 = *head;
-	stack_t *top2 = top1->next;
-	int temp = top1->n;
+	stack_t *top1;
+	stack_t *top2;
+	int temp;
 
 	(void)counter;
-	if (top1->next == NULL)
+	if (*head == NULL || !(*head)->next)
 		p_error0(6);
+	top1 = *head;
+	top2 = top1->next;
+	temp = top1->n;
 	top1->n = top2->n;
 	top2->n = temp;
 }
